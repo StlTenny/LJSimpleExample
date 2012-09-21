d62d544c9cbd173ff5f30cb69e7b0a813db5e239
Tennyson Hinds <StlTenny@gmail.com>
2012-09-21 12:19:31 -0400

    Switching to PayPal Express Library, original alpha commit

--- LJSimpleExample/LJAppDelegate.m
+++ LJSimpleExample/LJAppDelegate.m
@@ -27,7 +27,7 @@
     
     
     //Make sure to change api key to provided key
-    [LoopJoyStore initWithAPIKey:@"My-Api-Key" forEnv:LJ_ENV_SANDBOX withTarget:[LJMainViewController sharedController]];
+    [LoopJoyStore initWithAPIKey:@"iqEqGmZNfgtLFehwfry" forEnv:LJ_ENV_SANDBOX withTarget:[LJMainViewController sharedController]];
     return YES;
     
 }
